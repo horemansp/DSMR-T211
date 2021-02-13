7c6a5b7041bee31aa59e6bb2b9e4404e10787504
Patrik Horemans <horemansp@hotmail.com>
2021-02-13 19:38:14 +0100

    output of a telegram (for reference)

---
+++ output_dsmr_telegram.py
@@ -0,0 +1,23 @@
+b'\x00\n'
+b'/FLU5\\253769484_A\r\n'
+b'\r\n'
+b'0-0:96.1.4(50213)\r\n' = serie nummer van een gekoppelde meter op het 4de kanaal
+b'0-0:96.1.1(3153414733313030313332383938)\r\n' = serienummer van gekoppelde meter op kanaal 1
+b'0-0:1.0.0(210207135629W)\r\n' = real time verbruik tarief 1
+b'1-0:1.8.1(000858.642*kWh)\r\n' = afname to hiertoe tarief 1
+b'1-0:1.8.2(001264.926*kWh)\r\n' = afname tot hiertoe tarief 2
+b'1-0:2.8.1(000421.788*kWh)\r\n' = injectie tot hiertoe tarief 1 (dag)
+b'1-0:2.8.2(000162.755*kWh)\r\n' = injectie tot hiertoe tarief 2 (nacht/weekend)
+b'0-0:96.14.0(0002)\r\n' = tarief 1=dag=Hoog, 2=nacht/weekend=laag
+b'1-0:1.7.0(00.461*kW)\r\n' = real time verbruik
+b'1-0:2.7.0(00.000*kW)\r\n' = real time injectie
+b'1-0:32.7.0(222.8*V)\r\n' = real time spanning fase 1
+b'1-0:52.7.0(231.2*V)\r\n' = real time spanning fase 2
+b'1-0:72.7.0(228.6*V)\r\n' = real time spanning fase 3
+b'1-0:31.7.0(000*A)\r\n' = real time stroom fase 1
+b'1-0:51.7.0(002*A)\r\n' = real time stroom fase 2
+b'1-0:71.7.0(000*A)\r\n' = real time stroom fase 3
+b'0-0:96.3.10(1)\r\n'
+b'0-0:17.0.0(999.9*kW)\r\n' = vermogens begrenzing (999.9=geen begrenzing)
+b'1-0:31.4.0(999*A)\r\n' = stroombegrenzing (999=geen begrenzing)
+b'0-0:96.13.0()\r\n'
